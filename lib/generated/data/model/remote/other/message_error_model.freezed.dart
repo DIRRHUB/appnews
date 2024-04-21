beae039e4f5da1129c33b1918eec649c4f424585
DIRRHUB <bibishka117@gmail.com>
2024-04-21 22:31:18 +0300

    Push

--- lib/generated/data/model/remote/other/message_error_model.freezed.dart
+++ lib/generated/data/model/remote/other/message_error_model.freezed.dart
@@ -12,7 +12,7 @@ part of '../../../../../data/model/remote/other/message_error_model.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 MessageErrorModel _$MessageErrorModelFromJson(Map<String, dynamic> json) {
   return _MessageErrorModel.fromJson(json);
--- lib/generated/data/model/remote/suggest_categories/suggest_category_model.freezed.dart
+++ lib/generated/data/model/remote/suggest_categories/suggest_category_model.freezed.dart
@@ -12,7 +12,7 @@ part of '../../../../../data/model/remote/suggest_categories/suggest_category_mo
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
 
 SuggestCategoryModel _$SuggestCategoryModelFromJson(Map<String, dynamic> json) {
   return _SuggestCategoryModel.fromJson(json);
